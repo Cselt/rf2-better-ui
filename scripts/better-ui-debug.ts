c711c58b75e5151377922dc2e3b033acb4bf01a1
Tamas Cseh <tamas.cseh@cselt.hu>
2021-01-06 22:34:53 +0100

    Add debug version

--- scripts/better-ui-debug.ts
+++ scripts/better-ui-debug.ts
@@ -1,21 +1,10 @@
-function main(): void {
+function debugMain(): void {
   function addScriptTag(src: string): void {
     const script: HTMLScriptElement = document.createElement('script');
     script.src = src;
     script.defer = true;
     document.getElementsByTagName('body')[0].appendChild(script);
   }
-
-  addScriptTag('../framework/better-ui/runtime.<runtime>.js');
-  addScriptTag('../framework/better-ui/polyfills.<polyfills>.js');
-  addScriptTag('../framework/better-ui/main.<main>.js');
-
-  waitForElm('ui-view div').then(() => {
-    setTimeout(() => {
-      const rfBetterUi: HTMLElement = document.createElement('rf-better-ui');
-      document.getElementsByTagName('body')[0].prepend(rfBetterUi);
-    }, 200);
-  });
 
   function waitForElm(selector): Promise<any> {
     return new Promise(resolve => {
@@ -36,7 +25,21 @@ function main(): void {
       });
     });
   }
+
+  addScriptTag('http://localhost:4200/runtime.js');
+  addScriptTag('http://localhost:4200/polyfills.js');
+  addScriptTag('http://localhost:4200/styles.js');
+  addScriptTag('http://localhost:4200/vendor.js');
+  addScriptTag('http://localhost:4200/main.js');
+
+  waitForElm('ui-view div').then(() => {
+    setTimeout(() => {
+      const rfBetterUi: HTMLElement = document.createElement('rf-better-ui');
+      document.getElementsByTagName('body')[0].prepend(rfBetterUi);
+    }, 200);
+  });
 }
 
-main();
+debugMain();
+
 
