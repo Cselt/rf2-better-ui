f2493614ae34876b1bcf8c8abe1907886a38e23a
Tamas Cseh <tamas.cseh@cselt.hu>
2021-01-07 19:40:56 +0100

    Implement start arrow navigation in start-handler

--- projects/rf2-better-ui/src/app/start-handler/start-handler.component.ts
+++ projects/rf2-better-ui/src/app/start-handler/start-handler.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'rf-start-handler',
@@ -8,11 +8,45 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class StartHandlerComponent implements OnInit {
 
+  private listItems: NodeListOf<HTMLLIElement>;
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    if (!['ArrowRight', 'ArrowLeft'].includes(event.key)) {
+      return;
+    }
+
+    const selectedLi: HTMLLIElement = document.querySelector('ol.tabnavigation:not(.bottom) li.selected');
+    let selectedIndex: number = -1;
+    this.listItems.forEach((node: HTMLLIElement, index: number) => {
+      if (selectedLi === node) {
+        selectedIndex = index;
+      }
+    });
+
+    switch (event.key) {
+      case 'ArrowRight':
+        selectedIndex = Math.min(selectedIndex + 1, this.listItems.length - 1);
+        break;
+
+      case 'ArrowLeft':
+        selectedIndex = Math.max(selectedIndex - 1, 0);
+        break;
+    }
+
+    this.listItems.forEach((node: HTMLLIElement, index: number) => {
+      if (index === selectedIndex) {
+        node.click();
+      }
+    });
+  }
+
   constructor() {
-    console.log("Start handler activated");
+    console.log('Start handler activated');
   }
 
   ngOnInit(): void {
+    this.listItems = document.querySelectorAll('ol.tabnavigation:not(.bottom) li');
   }
 
 }
