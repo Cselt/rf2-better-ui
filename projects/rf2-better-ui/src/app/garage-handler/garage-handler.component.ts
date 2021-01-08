df5ba9ea7d8a513e08b5d433efc9327bca43506b
Tamas Cseh <tamas.cseh@cselt.hu>
2021-01-08 21:01:26 +0100

    Highlight current track folder in setup poup

--- projects/rf2-better-ui/src/app/garage-handler/garage-handler.component.ts
+++ projects/rf2-better-ui/src/app/garage-handler/garage-handler.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
-import { arrowNavigation } from '../utils/utils';
+import { arrowNavigation, waitForElement } from '../utils/utils';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'rf-garage-handler',
@@ -10,16 +11,39 @@ import { arrowNavigation } from '../utils/utils';
 export class GarageHandlerComponent implements OnInit {
 
   private listItems: NodeListOf<HTMLLIElement>;
+  private currentTrackFolder: string;
 
   @HostListener('window:keyup', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
-    return arrowNavigation(event, this.listItems, "main section div.selected");
+    return arrowNavigation(event, this.listItems, 'main section div.selected');
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.listItems = document.querySelectorAll('main section div.thumbnail');
+
+    this.findSetupButtons();
+    this.loadActiveTrack();
   }
 
+  private findSetupButtons(): void {
+    document.querySelectorAll('left-section button').forEach((button: HTMLButtonElement) =>
+      button.onclick = () => {
+        waitForElement('.modal-dialog .setup-content .setup-tree-wrapper ul').then((element: HTMLUListElement) => {
+          element.querySelectorAll('li .setup-name').forEach((span: HTMLSpanElement) => {
+            if (span.textContent === this.currentTrackFolder) {
+              span.textContent += '*';
+              span.classList.add('track-selected');
+            }
+          });
+        });
+      });
+  }
+
+  private loadActiveTrack(): void {
+    this.http.get('/rest/garage/currentTrackFolder', {responseType: 'text'})
+      .subscribe((folder: string) => this.currentTrackFolder = folder);
+  }
 }
