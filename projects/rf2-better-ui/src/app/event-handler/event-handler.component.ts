1d9605916e9f4226c7f8612a8c422aeab68c6e2f
Tamas Cseh <tamas.cseh@cselt.hu>
2021-01-09 19:03:57 +0100

    Fix: display rf-chat on tab change

--- projects/rf2-better-ui/src/app/event-handler/event-handler.component.ts
+++ projects/rf2-better-ui/src/app/event-handler/event-handler.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { waitForElement } from '../utils/utils';
 
 @Component({
@@ -7,16 +7,49 @@ import { waitForElement } from '../utils/utils';
   styleUrls: ['./event-handler.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class EventHandlerComponent implements OnInit {
+export class EventHandlerComponent implements OnInit, OnDestroy {
+
+  private tabHolder: HTMLDivElement;
+  private observer: MutationObserver;
 
   ngOnInit(): void {
     // add rf-chat component to the DOM
     waitForElement('.camera-wrapper').then(() => {
-      const ngIncludeElement: HTMLElement = document.querySelector('.camera-wrapper').parentElement;
-      ngIncludeElement.classList.add('cameraHolder');
-
-      const rfChat: HTMLElement = document.createElement('rf-chat');
-      ngIncludeElement.appendChild(rfChat);
+      this.tabHolder = document.querySelector("div[ng-if='eventCtrl.gameState.navigationState']");
+      this.injectChat();
+      this.observeTabChange();
     });
+  }
+
+  private observeTabChange(): void {
+    this.observer = new MutationObserver((mutations: MutationRecord[]) => {
+      for (const m of mutations) {
+        // if class changed and the selected tab is camera
+        if (m.attributeName === 'class' && this.tabHolder.classList.contains('tab-camera')) {
+          this.injectChat();
+          break;
+        }
+      }
+    });
+
+    this.observer.observe(this.tabHolder, {attributes: true});
+  }
+
+  private injectChat(): void {
+    const ngIncludeElement: HTMLElement = document.querySelector('.camera-wrapper').parentElement;
+
+    if (!!ngIncludeElement.querySelector('rf-chat')) {
+      // already injected
+      return;
+    }
+
+    ngIncludeElement.classList.add('cameraHolder');
+
+    const rfChat: HTMLElement = document.createElement('rf-chat');
+    ngIncludeElement.appendChild(rfChat);
+  }
+
+  ngOnDestroy(): void {
+    this.observer.disconnect();
   }
 }
