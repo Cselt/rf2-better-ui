738d92c1b6fc304505c84c12e4cac403fd50695b
Tamas Cseh <tamas.cseh@cselt.hu>
2021-01-10 17:28:23 +0100

    Add: read version information from pacakge.json

--- projects/rf2-better-ui/src/app/better-ui/better-ui.component.ts
+++ projects/rf2-better-ui/src/app/better-ui/better-ui.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../../../package.json';
 
 @Component({
   selector: 'rf-better-ui',
@@ -11,7 +12,7 @@ export class BetterUiComponent implements OnInit {
     console.log('Better UI loaded');
 
     const div: HTMLDivElement = document.createElement('div');
-    div.innerHTML = `<span style="position: absolute; bottom: 0; right: 0; z-index: 1; font-size: 10px">Better-UI v2.1.0</span>`;
+    div.innerHTML = `<span style="position: absolute; bottom: 0; right: 0; z-index: 1; font-size: 10px">Better-UI ${version}</span>`;
     document.getElementsByTagName('body')[0].prepend(div);
 
     this.applyHandlers();
